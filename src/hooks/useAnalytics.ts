f26edb6177a54b92a9176601a3e3440b67df432d
Ivan Saranchonkau <i.saranchonkau@ozitag.com>
2020-09-12 18:22:33 +0300

    v0.1.2 - Add resolving of analytics keys (env, backend, user's override)

--- src/hooks/useAnalytics.ts
+++ src/hooks/useAnalytics.ts
@@ -2,27 +2,62 @@ import { useEffect, useRef } from 'react';
 import TagManager from 'react-gtm-module';
 import { Router } from 'next/router';
 
+import { Nullable } from '@tager/web-core';
+
 import { getAnalyticsSettings } from '../services/requests';
 import googleAnalytics from '../services/googleAnalytics';
 import yandexMetrika from '../services/yandexMetrika';
 import facebookPixel from '../services/facebookPixel';
 import { AnalyticsSettingsType } from '../typings/model';
 
+type Keys = Partial<AnalyticsSettingsType>;
+
+function getEnvKeys(): Keys {
+  return {
+    googleTagManagerId: process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID,
+    googleAnalyticsId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID,
+    yandexCounterId: process.env.NEXT_PUBLIC_YANDEX_METRIKA_COUNTER_ID,
+    facebookPixelId: process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID,
+  };
+}
+
+function createKeyResolver(params: {
+  useBackend: boolean;
+  userKeys: Keys;
+  backendKeys: Keys;
+}): (name: keyof Keys) => Nullable<string> {
+  const envKeys = getEnvKeys();
+
+  return function resolveAnalyticsKey(name) {
+    if (Object.keys(params.userKeys).includes(name)) {
+      return params.userKeys[name] ?? null;
+    }
+
+    const valueFromBackend = params.useBackend
+      ? params.backendKeys[name]
+      : null;
+
+    const valueFromEnv = envKeys[name];
+
+    return valueFromBackend || valueFromEnv || null;
+  };
+}
+
 type Options = {
   timeout?: number;
-  keys?: Partial<AnalyticsSettingsType>;
+  keys?: Keys;
   useBackend?: boolean;
 };
 
-function useAnalytics(userOptions?: Options) {
-  const optionsRef = useRef<Options>({
-    useBackend: userOptions?.useBackend ?? true,
-    timeout: userOptions?.timeout ?? 2000,
-    keys: userOptions?.keys ?? {},
-  });
+function useAnalytics(userOptions: Options = {}) {
+  const optionsRef = useRef<Options>(userOptions);
 
   useEffect(() => {
     const { current: options } = optionsRef;
+
+    const timeout = options.timeout ?? 2000;
+    const useBackend = options.useBackend ?? true;
+    const userKeys = options.keys ?? {};
 
     const routeChangeListenerList: Array<() => void> = [];
 
@@ -33,39 +68,52 @@ function useAnalytics(userOptions?: Options) {
     function initTrackers(): void {
       const settingsPromise: Promise<Partial<
         AnalyticsSettingsType
-      >> = options.useBackend
+      >> = useBackend
         ? getAnalyticsSettings().then((response) => response.data)
         : Promise.resolve({});
 
       settingsPromise
-        .then((settings) => {
-          const keys: Partial<AnalyticsSettingsType> = {
-            ...settings,
-            ...options.keys,
-          };
+        .then((backendKeys) => {
+          const resolveKey = createKeyResolver({
+            useBackend,
+            userKeys,
+            backendKeys,
+          });
 
-          if (keys.googleTagManagerId) {
+          /** Google Tag Manager */
+          const googleTagManagerId = resolveKey('googleTagManagerId');
+
+          if (googleTagManagerId) {
             TagManager.initialize({
-              gtmId: keys.googleTagManagerId,
+              gtmId: googleTagManagerId,
             });
           }
 
-          if (keys.googleAnalyticsId) {
-            googleAnalytics.init(keys.googleAnalyticsId);
+          /** Google Analytics */
+          const googleAnalyticsId = resolveKey('googleAnalyticsId');
+
+          if (googleAnalyticsId) {
+            googleAnalytics.init(googleAnalyticsId);
             googleAnalytics.trackPageView();
 
             routeChangeListenerList.push(() => googleAnalytics.trackPageView());
           }
 
-          if (keys.yandexCounterId) {
-            yandexMetrika.init(keys.yandexCounterId);
+          /** Yandex Metrika */
+          const yandexCounterId = resolveKey('yandexCounterId');
+
+          if (yandexCounterId) {
+            yandexMetrika.init(yandexCounterId);
             yandexMetrika.trackPageView();
 
             routeChangeListenerList.push(() => yandexMetrika.trackPageView());
           }
 
-          if (keys.facebookPixelId) {
-            facebookPixel.init(keys.facebookPixelId);
+          /** Facebook Pixel */
+          const facebookPixelId = resolveKey('facebookPixelId');
+
+          if (facebookPixelId) {
+            facebookPixel.init(facebookPixelId);
             facebookPixel.trackPageView();
 
             routeChangeListenerList.push(() => facebookPixel.trackPageView());
@@ -76,7 +124,7 @@ function useAnalytics(userOptions?: Options) {
         });
     }
 
-    setTimeout(initTrackers, options.timeout);
+    setTimeout(initTrackers, timeout);
 
     Router.events.on('routeChangeComplete', handleRouteChangeComplete);
 
